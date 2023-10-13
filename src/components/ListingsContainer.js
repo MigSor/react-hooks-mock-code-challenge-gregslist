95539e1da5e50b5e4127ea7780a1f7d6417d618c
Miguel Soriano <miguel.sor.88@gmail.com>
2023-10-13 14:47:57 +1000

    first commit, made fetch to db.json and mapped to listing card component

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,26 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
+import { useEffect, useState } from "react";
 
 function ListingsContainer() {
-  return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
-  );
+   const [listings, setListings] = useState([]);
+   useEffect(() => {
+      fetch("http://localhost:6001/listings")
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+            setListings(data);
+         });
+   }, []);
+   return (
+      <main>
+         <ul className="cards">
+            {listings.map((listing) => {
+               return <ListingCard key={listing.id} listing={listing} />;
+            })}
+         </ul>
+      </main>
+   );
 }
 
 export default ListingsContainer;
